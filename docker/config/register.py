751cb858981bb9fe1103de96f885fc5b0842bff0
forest1040 <forest1040@gmail.com>
2025-10-28 09:43:47 +0900

    Add script to register possible links.

--- docker/config/register.py
+++ docker/config/register.py
@@ -11,3 +11,13 @@ cxn = labrad.connect()
 reg = cxn.registry
 reg.cd(["Servers", "Data Vault", "Repository"])
 reg.set("kappa_docker", "/root/labrad-data")
+
+# load qube ip info
+reg.cd(["", "Servers", "QuBE"])
+with open("/root/config/possible_links.json", encoding="utf-8") as f:
+    possible_links_dict = json.load(f)
+    reg.set("possible_links", json.dumps(possible_links_dict))
+
+with open("/root/config/chassis_skew.json", encoding="utf-8") as f:
+    possible_links_dict = json.load(f)
+    reg.set("chassis_skew", json.dumps(possible_links_dict))
